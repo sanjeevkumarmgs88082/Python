1a226a42280acab15a3841c9f3da9d15d9fd0522
sanjeevkumarmgs88082 <39999935+sanjeevkumarmgs88082@users.noreply.github.com>
2019-08-25 10:05:55 +0530

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -4,3 +4,6 @@ git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/sanjeevkumarmgs88082/Python.git
 git push -u origin master
+
+#Simple Code
+print("Hello This is the first python programming")
